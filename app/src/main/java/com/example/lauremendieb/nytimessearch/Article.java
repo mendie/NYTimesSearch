94c83c07a374a67429d2884309c352835035c3f2
mendie <annilaure2014@icloud.com>
2017-07-27 18:50:05 -0400

    second commit

---
+++ app/src/main/java/com/example/lauremendieb/nytimessearch/Article.java
@@ -0,0 +1,65 @@
+package com.example.lauremendieb.nytimessearch;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+/**
+ * Created by lauremendieb on 27/07/2017.
+ */
+
+public class Article {
+
+    String webUrl;
+    String headlines;
+    String thumbNail;
+
+    public String getWebUrl() {
+        return webUrl;
+    }
+
+    public String getHeadlines() {
+        return headlines;
+    }
+
+    public String getThumbNail() {
+        return thumbNail;
+    }
+
+
+
+    public Article(JSONObject jsonObject) {
+        try {
+            this.webUrl = jsonObject.getString("web_url");
+            this.headlines = jsonObject.getJSONObject("headline").getString("main");
+
+            JSONArray multimedia = jsonObject.getJSONArray("multimedia");
+
+            if (multimedia.length() > 0){
+                JSONObject multimediaJason = multimedia.getJSONObject(0);
+                this.thumbNail = "http://www.nytimes.com/" + multimediaJason.getString("url");
+            }else {
+                this.thumbNail = "";
+            }
+        }catch (JSONException e) {
+
+        }
+
+    }
+
+    public static ArrayList<Article> fromJSONArray(JSONArray array) {
+        ArrayList<Article> results = new ArrayList<>();
+
+        for (int x = 0; x <array.length(); x++) {
+            try {
+               results.add(new Article(array.getJSONObject(x)));
+            }catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return results;
+    }
+}
--- app/src/main/java/com/example/lauremendieb/nytimessearch/SearchActivity.java
+++ app/src/main/java/com/example/lauremendieb/nytimessearch/SearchActivity.java
@@ -3,19 +3,33 @@ package com.example.lauremendieb.nytimessearch;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.GridView;
-import android.widget.Toast;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import cz.msebera.android.httpclient.Header;
 
 public class SearchActivity extends AppCompatActivity {
 
     EditText etQuery;
     GridView gvResults;
     Button btnSearch;
+
+    ArrayList<Article> articles;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +46,8 @@ public class SearchActivity extends AppCompatActivity {
         etQuery = (EditText) findViewById(R.id.etQuery);
         gvResults = (GridView) findViewById(R.id.gvResults);
         btnSearch = (Button) findViewById(R.id.btnSearch);
+        articles = new ArrayList<>();
+        
     }
 
     @Override
@@ -57,7 +73,32 @@ public class SearchActivity extends AppCompatActivity {
     }
 
     public void onArticleSearch(View view) {
-        String query = etQuery.getText().toString();
-        Toast.makeText(this, "Searching for" + query,Toast.LENGTH_LONG).show();
+        final String query = etQuery.getText().toString();
+
+        //Toast.makeText(this, "Searching for" + query,Toast.LENGTH_LONG).show();
+        AsyncHttpClient client = new AsyncHttpClient();
+        String url = "http://api.nytimes.com/svc/search/v2/articlesearch.json";
+
+        RequestParams params = new RequestParams();
+        params.put("api-key", "e8f04de786394fa682df696bb0e0e16d");
+        params.put("page",0);
+        params.put("q",query);
+
+        client.get(url,params, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                Log.d("DEBUG", response.toString());
+                //Toast.makeText(SearchActivity.this, "" + response.toString(), Toast.LENGTH_SHORT).show();
+                JSONArray articleJsonResults = null;
+
+                try {
+                    articleJsonResults = response.getJSONObject("response").getJSONArray("docs");
+                    Log.d("DEBUG", articleJsonResults.toString());
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
     }
 }
